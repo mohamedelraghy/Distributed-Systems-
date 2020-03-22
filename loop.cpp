ce18e53067478cbd59f4c2e2499c86be88afd070
mohamedelraghy <elraghy8@gmail.com>
2020-03-22 13:40:43 +0200

    log the submssion and it's thread num

--- loop.cpp
+++ loop.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 typedef vector<int> vi;
+typedef long long ll;
 
 int main(){
 
@@ -11,11 +12,17 @@ int main(){
     #pragma omp parallel
     {
         vi vec1, vec2;
-        #pragma omp for
-        for(int i = 0; i < 1000; i++) vec1.push_back(i + 1);
-        vec2 = vec1;
-        
-        for(auto i : vec2) cout << i << endl;
+        ll Sum = 0;
+        #pragma omp for{
+
+            for(int i = 0; i < 1000; i++) vec1.push_back(i + 1);
+            vec2 = vec1;
+
+            for(int i = 0; i < 1000; i++){
+                cout << "Submsion of two valuse = " << vec1[i] + vec2[i] << " and it's RUN at THREAD NO. = " << omp_get_thread_num() << endl;
+                Sum += (vec1[i] + vec2[i]);
+            } 
+        }   
     }
             
 }
