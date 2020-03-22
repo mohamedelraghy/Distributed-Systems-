4065a7f89ce8401101652f9404b024ae02a2126b
mohamedelraghy <elraghy8@gmail.com>
2020-03-22 13:58:49 +0200

    put for directive in shourtcut style

--- loop.cpp
+++ loop.cpp
@@ -3,26 +3,24 @@
 
 using namespace std;
 
-typedef vector<int> vi;
 typedef long long ll;
+typedef vector<ll> vll;
 
 int main(){
 
+    vll vec1, vec2;
+    ll Sum = 0;
 
-    #pragma omp parallel
-    {
-        vi vec1, vec2;
-        ll Sum = 0;
-        #pragma omp for{
+    for(int i = 0; i < 1000; i++) vec1.push_back(i + 1);
+    vec2 = vec1;
 
-            for(int i = 0; i < 1000; i++) vec1.push_back(i + 1);
-            vec2 = vec1;
-
-            for(int i = 0; i < 1000; i++){
-                cout << "Submssion of two valuse = " << vec1[i] + vec2[i] << " and it's RUN at THREAD NO. = " << omp_get_thread_num() << endl;
-                Sum += (vec1[i] + vec2[i]);
-            } 
-        }   
-    }
+    #pragma omp parallel for shared(vec1, vec2, Sum)
+                   
+        for(int i = 0; i < 1000; i++){
+            cout << "\nSubmsion of two valuse = " << vec1[i] + vec2[i] << "\nand it's RUN at THREAD NO. = " << omp_get_thread_num() << endl;
+            Sum += (vec1[i] + vec2[i]);
+        }    
+    
+    cout << "Sum = " << Sum << endl;
             
 }
